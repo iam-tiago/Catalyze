c9f569f4d302772d4baaa3b97f49c76e1d654829
Tiago Canabarro <canabarro.engineer@gmail.com>
2026-05-03 18:35:26 -0300

    Initial Commit

---
+++ Catalyze/CatalyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalyzeApp.swift
+//  Catalyze
+//
+//  Created by Tiago Canabarro on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CatalyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
